f575a9c67216db9bec8abbbe5c21d53a56f9294b
Ekansh Narayan Mishra <ekanshaman2512@gmail.com>
2025-07-16 21:27:07 +0530

    Fix: Updated API endpoint to Render backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://online-code-compiler-cr6y.onrender.com";
 
 export const fetchSharedCode = (id) => {
   return axios.get(`${BASE_URL}/code/${id}`);
