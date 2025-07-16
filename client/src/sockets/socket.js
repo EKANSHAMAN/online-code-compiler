51095eb990f6d3611359121bc993c56653ae9af5
Ekansh Narayan Mishra <ekanshaman2512@gmail.com>
2025-07-16 21:59:00 +0530

    Fix: Updated Socket.io URL to deployed backend

--- client/src/sockets/socket.js
+++ client/src/sockets/socket.js
@@ -1,3 +1,7 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000");
+
+const socket = io("https://online-code-compiler-cr6y.onrender.com", {
+  transports: ["websocket"],
+});
+
 export default socket;
